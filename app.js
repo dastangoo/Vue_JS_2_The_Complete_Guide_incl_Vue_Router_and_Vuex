4f5c176c64d0d134e7844f7c35054117a381ac8b
dastangoo <hamed.dastangoo@gmail.com>
2019-07-21 15:54:15 +0430

    Listening to Events

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 new Vue({
 	el: '#app',
 	data: {
-        title: 'Hello World!',
-        link: 'http://google.com',
-        finishedLink: '<a href="http://google.com">Google</a>'
+        counter: 0
     },
     methods: {
-        sayHello: function(){
-            //return 'Hello!';
-            this.title = 'Hello';
-            return this.title;
+        increase: function(){
+           this.counter++; 
         }
     }
 })
