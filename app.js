3df60b27056be8bbd21c6b197a5a3b99e35dc366
dastangoo <hamed.dastangoo@gmail.com>
2019-07-22 13:53:00 +0430

    Modifying an Event - with Event Modifiers

--- app.js
+++ app.js
@@ -13,5 +13,8 @@ new Vue({
             this.x = event.clientX;
             this.y = event.clientY;
         }
+        //dummy: function(event){
+            //event.stopPropagation();
+        //}
     }
 })
