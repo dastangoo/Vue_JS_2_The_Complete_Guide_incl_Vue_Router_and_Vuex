5e22def9845a77d8140f94f942cea8f3426fa747
dastangoo <hamed.dastangoo@gmail.com>
2019-07-22 15:07:03 +0430

    Using Two-way Binding

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 new Vue({
 	el: '#app',
 	data: {
-        counter: 0,
-        x: 0,
-        y: 0
-    },
-    methods: {
-        increase: function(step, event){
-           this.counter += step; 
-        },
-        updateCoordinate: function(event){
-            this.x = event.clientX;
-            this.y = event.clientY;
-        },
-        //dummy: function(event){
-            //event.stopPropagation();
-        //}
-        alertMe: function(){
-            alert('Alert!');
-        }
+        name: 'Jack'
     }
 })
