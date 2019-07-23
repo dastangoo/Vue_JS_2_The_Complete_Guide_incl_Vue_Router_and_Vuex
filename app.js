d1561c08b17b981d7a1095cf0121305221fddcfc
dastangoo <hamed.dastangoo@gmail.com>
2019-07-23 09:05:33 +0430

    An Alternative to Computed Properties - Watching for Changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 new Vue({
-	el: '#app',
-	data: {
+    el: '#app',
+    data: {
         counter: 0,
         secondCounter: 0
     },
@@ -8,6 +8,14 @@ new Vue({
         output: function(){
             console.log('Computed');
             return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
+        }
+    },
+    watch: {
+        counter: function(value){
+            var vm = this;
+            setTimeout(function(){
+                vm.counter = 0;    
+            }, 2000);
         }
     },
     methods: {
