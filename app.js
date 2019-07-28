d6e6feaed5721c554df54fdfd1fbd8c1fa501315
dastangoo <hamed.dastangoo@gmail.com>
2019-07-28 13:23:32 +0430

    Placing  and Using them on your Templates

--- app.js
+++ app.js
@@ -3,27 +3,30 @@ var data = {
     showParagraph: false
 }
 var vm1 = new Vue({
-  el: '#app1',
-  data: data,
-  methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
+    el: '#app1',
+    data: data,
+    methods: {
+        show: function() {
+            this.showParagraph = true;
+            this.updateTitle('The VueJS Instance (Updated)');
+            //console.log(this.$refs);
+            //console.log(this.$refs.myButton);
+            this.$refs.myButton.innerText = 'Test';
+        },
+        updateTitle: function(title) {
+            this.title = title;
+        }
     },
-    updateTitle: function(title) {
-      this.title = title;
+    computed: {
+        lowercaseTitle: function() {
+            return this.title.toLowerCase();
+        }
+    },
+    watch: {
+        title: function(value) {
+            alert('Title changed, new value: ' + value);
+        }
     }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
-    }
-  }
 });
 
 console.log(vm1);
@@ -31,19 +34,21 @@ console.log(vm1.title);
 console.log(vm1.$data.title);
 console.log(vm1.$data === data);
 
+vm1.$refs.heading.innerText = 'Something else';
+
 setTimeout(function(){
     vm1.title = 'Changed by Timer';
     vm1.show();
 }, 3000);
 
 var vm2 = new Vue({
-  el: '#app2',
-  data: {
-    title: 'The Second Instance'
-  },
-  methods: {
-      onChange: function(){
-          vm1.title = 'Changed';
-      }
-  }
+    el: '#app2',
+    data: {
+        title: 'The Second Instance'
+    },
+    methods: {
+        onChange: function(){
+            vm1.title = 'Changed';
+        }
+    }
 });
