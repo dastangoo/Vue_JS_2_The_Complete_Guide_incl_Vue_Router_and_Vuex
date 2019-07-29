eafea12b39c611f85e7f7ff29424cc6f5dbb84ca
dastangoo <hamed.dastangoo@gmail.com>
2019-07-29 10:41:08 +0430

    Module Introduction

--- app.js
+++ app.js
@@ -2,5 +2,17 @@ new Vue({
     el: '#app',
     data: {
         title: 'The VueJS Instance'
+    },
+    beforeCreate: function(){
+        console.log('beforeCreate()');
+    },
+    created: function(){
+        console.log('created()');
+    },
+    beforeMount: function(){
+        console.log('beforeMount()');        
+    },
+    destroy: function(){
+        console.log('destroy()');
     }
 });
