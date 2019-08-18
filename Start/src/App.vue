f26ec1e5943f76e66a187a8d3b0a8dfb347bf5fc
dastangoo <hamed.dastangoo@gmail.com>
2019-08-18 10:53:38 +0430

    Initializing the Application

--- Start/src/App.vue
+++ Start/src/App.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="container">
-    
+        <p>{{ quotes }}</p>
+        <p>{{ maxQuotes }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data: function() {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10    
+            }
+        }
     }
 </script>
 
