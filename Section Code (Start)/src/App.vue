a5f3249d7cf13fc0908c2e30f0d41befab1ca96b
dastangoo <hamed.dastangoo@gmail.com>
2019-09-15 19:56:20 +0430

    Global Filters and How to Chain Multiple Filters

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUpperCase }}</p>
+                <p>{{ text | to-uppercase | to-lowercase}}</p>
             </div>
         </div>
     </div>
@@ -17,7 +17,8 @@
             }
         },
         filters: {
-            toUpperCase(value) {
+            'to-uppercase'(value) {
+            // toUpperCase(value) {
                 return value.toUpperCase();
             }
         }
