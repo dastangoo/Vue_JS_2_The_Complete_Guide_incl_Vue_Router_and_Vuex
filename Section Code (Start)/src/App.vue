a9c4fcd711e49b6b43fd49715b0a783369116f08
dastangoo <hamed.dastangoo@gmail.com>
2019-09-20 13:04:42 +0430

    Creating an Animation in JavaScript

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -12,20 +12,20 @@
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br><br>
                 <transition :name="alertAnimation">
-                    <div class="alert alert-info" v-show="show">This is some Info</div>
+                <div class="alert alert-info" v-show="show">This is some Info</div>
                 </transition>
                 <transition name="slide" type="animation" appear>
-                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                <div class="alert alert-info" v-if="show">This is some Info</div>
                 </transition>
-                <transition 
-                    enter-active-class="animated bounce"
-                    leave-active-class="animated shake"
-                >
-                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                <transition
+                                              enter-active-class="animated bounce"
+                                              leave-active-class="animated shake"
+                                              >
+                                              <div class="alert alert-info" v-if="show">This is some Info</div>
                 </transition>
                 <transition :name="alertAnimation" mode="out-in">
-                    <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
-                    <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
+                <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
+                <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
                 </transition>
                 <hr>
                 <button class="btn btn-primary" @click="load = !load">Leave / Remove Element</button>
@@ -40,8 +40,8 @@
                         @after-leave="afterLeave"
                         @leave-cancelled="leaveCancelled"
                         :css="false">
-                    <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load">
-                    </div>
+                <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load">
+                </div>
                 </transition>
             </div>
         </div>
@@ -49,98 +49,119 @@
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                show: false,
-                load: true,
-                alertAnimation: 'fade'
-            }
+export default {
+    data() {
+        return {
+            show: false,
+            load: true,
+            alertAnimation: 'fade',
+            elementWidth: 100
+        }
+    },
+    methods: {
+        beforeEnter(el) {
+            console.log('beforeEnter');
+            this.elementWidth = 100;
+            el.style.width = this.elementWidth + 'px';
         },
-        methods: {
-            beforeEnter(el) {
-                console.log('beforeEnter');
-            },
-            enter(el, done) {
-               console.log('enter');
-               done();
-            },
-            afterEnter(el) {
-                console.log('afterEnter');
-            },
-            enterCancelled(el) {
-                console.log('enterCancelled');
-            },
-            beforeLeave(el) {
-                console.log('beforeLeave');
-            },
-            leave(el, done) {
-                console.log('leave');
-                done();
-            },
-            afterLeave(el) {
-                console.log('afterLeave');
-            },
-            leaveCancelled(el) {
-                console.log('leaveCancelled');
-            }
+        enter(el, done) {
+            console.log('enter');
+            let round = 1;
+            const interval = setInterval(() => {
+                el.style.width = (this.elementWidth + round * 10) + 'px';
+                round++;
+                if (round > 20) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 20);
+        },
+        afterEnter(el) {
+            console.log('afterEnter');
+        },
+        enterCancelled(el) {
+            console.log('enterCancelled');
+        },
+        beforeLeave(el) {
+            console.log('beforeLeave');
+            this.elementWidth = 300;
+            el.style.width = this.elementWidth + 'px';
+        },
+        leave(el, done) {
+            console.log('leave');
+            let round = 1;
+            const interval = setInterval(() => {
+                el.style.width = (this.elementWidth - round * 10) + 'px';
+                round++;
+                if (round > 20) {
+                    clearInterval(interval);
+                    done();
+                }
+            }, 20);
+        },
+        afterLeave(el) {
+            console.log('afterLeave');
+        },
+        leaveCancelled(el) {
+            console.log('leaveCancelled');
         }
     }
+}
 </script>
 
 <style>
-    .fade-enter {
-        opacity: 0;
-    }
+.fade-enter {
+    opacity: 0;
+}
 
-    .fade-enter-active {
-        transition: opacity 1s;
-        /* opacity: 1 */
-    }
+.fade-enter-active {
+    transition: opacity 1s;
+    /* opacity: 1 */
+}
 
-    .fade-leave {
-        /* opacity: 1; */
-    }
-    
-    .fade-leave-active {
-        transition: opacity 1s;
-        opacity: 0;
-    }
+.fade-leave {
+    /* opacity: 1; */
+}
 
-    .slide-enter {
-        opacity: 0;
-        /* transform: translateY(20px); */
-    }
+.fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+}
 
-    .slide-enter-active {
-        animation: slide-in 1s ease-out forwards;
-        transition: opacity .5s;
-    }
+.slide-enter {
+    opacity: 0;
+    /* transform: translateY(20px); */
+}
 
-    .slide.leave {
+.slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+}
 
-    }
+.slide.leave {
 
-    .slide.leave-active {
-        animation: slide-out 1s ease-out forwards;
-        transition: opacity 1s;
-        opacity: 0;
-    }
+}
 
-    @keyframes slide-in {
-        from {
-            transform: translateY(20px);
-        }
-        to {
-            transform: translateY(0);
-        }
+.slide.leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity: 0;
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateY(20px);
     }
-    @keyframes slide-out {
-        from {
-            transform: translateY(0);
-        }
-        to {
-            transform: translateY(20px);
-        }
+    to {
+        transform: translateY(0);
     }
+}
+@keyframes slide-out {
+    from {
+        transform: translateY(0);
+    }
+    to {
+        transform: translateY(20px);
+    }
+}
 </style>
