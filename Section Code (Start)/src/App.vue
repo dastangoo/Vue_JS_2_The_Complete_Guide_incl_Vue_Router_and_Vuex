6cffc1e2933a762954e8a9d898b749e107f6b450
dastangoo <hamed.dastangoo@gmail.com>
2019-09-21 22:12:53 +0430

    POSTing Data to a Server (Sending a POST Request)

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -29,7 +29,13 @@
         },
         methods: {
             submit() {
-                console.log(this.user);
+                // console.log(this.user);
+                this.$http.post('https://vuejs-http-1429f.firebaseio.com/data.json', this.user)
+                    .then(response => {
+                        console.log(response);
+                    }, error => {
+                        console.log(error);
+                    });
             }
         }
     }
