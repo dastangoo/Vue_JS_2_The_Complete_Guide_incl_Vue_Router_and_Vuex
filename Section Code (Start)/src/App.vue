00736dfe7e0c072e9571bc3d032bd9a8824ff9b4
dastangoo <hamed.dastangoo@gmail.com>
2019-09-21 22:48:53 +0430

    Configuring vue-resource Globally

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -37,7 +37,7 @@
         methods: {
             submit() {
                 //console.log(this.user);
-                this.$http.post('https://vuejs-http-1429f.firebaseio.com/data.json', this.user)
+                this.$http.post('', this.user)
                     .then(response => {
                         console.log(response);
                     }, error => {
@@ -45,7 +45,7 @@
                     });
             },
             fetchData() {
-                this.$http.get('https://vuejs-http-1429f.firebaseio.com/data.json')
+                this.$http.get('')
                     .then(response => {
                         return response.json();
                     })
