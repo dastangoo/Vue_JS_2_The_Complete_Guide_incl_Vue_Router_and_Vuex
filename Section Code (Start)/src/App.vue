dcf1254c6788414fd5daf38b5ffffae3013aa9e9
dastangoo <hamed.dastangoo@gmail.com>
2019-09-22 21:13:05 +0330

    Understanding Template URLs

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -13,30 +13,33 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
-                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
-                 <br><br>
-                 <ul class="list-group">
-                     <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
-                 </ul>
+                <input class="form-control" type="text" v-model="node">
+                <br><br>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                user: {
-                    username: '',
-                    email: ''
-                },
-                users: [],
-                resource: {}
-            };
-        },
-        methods: {
-            submit() {
+export default {
+    data() {
+        return {
+            user: {
+                username: '',
+                email: ''
+            },
+            users: [],
+            resource: {},
+            node: 'data'
+        };
+    },
+    methods: {
+        submit() {
             //    this.$http.post('data.json', this.user)
             //        .then(response => {
             //            console.log(response);
@@ -44,29 +47,41 @@
             //            console.log(error);
             //        });
             //    this.resource.save({}, this.user);
-                this.resource.saveAlt(this.user);
-            },
-            fetchData() {
-                this.$http.get('data.json')
-                    .then(response => {
-                        return response.json();
-                    })
-                    .then(data => {
-                        const resultArray = [];
-                        for (let key in data) {
-                            resultArray.push(data[key]);
-                        }
-                        this.users = resultArray;
-                    });
-            }
+            this.resource.saveAlt(this.user);
         },
-        created() {
-            const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'}
-            };
-            this.resource = this.$resource('data.json', {}, customActions);
+        fetchData() {
+            //this.$http.get('data.json')
+            //.then(response => {
+            //    return response.json();
+            //})
+            //.then(data => {
+            //    const resultArray = [];
+            //    for (let key in data) {
+            //        resultArray.push(data[key]);
+            //    }
+            //    this.users = resultArray;
+            //});
+            this.resource.getData({node: this.node})
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    const resultArray = [];
+                    for (let key in data) {
+                        resultArray.push(data[key]);
+                    }
+                    this.users = resultArray;
+                });
         }
+    },
+    created() {
+        const customActions = {
+            saveAlt: {method: 'POST', url: 'alternative.json'},
+            getData: {method: 'GET', }
+        };
+        this.resource = this.$resource('{node}.json', {}, customActions);
     }
+}
 </script>
 
 <style>
