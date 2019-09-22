4ae6e836339f320ddd30e546c597f789b7d86935
dastangoo <hamed.dastangoo@gmail.com>
2019-09-22 20:49:10 +0330

    Where the resource in vue-resource Comes From

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -31,21 +31,22 @@
                     username: '',
                     email: ''
                 },
-                users: []
+                users: [],
+                resource: {}
             };
         },
         methods: {
             submit() {
-                //console.log(this.user);
-                this.$http.post('', this.user)
-                    .then(response => {
-                        console.log(response);
-                    }, error => {
-                        console.log(error);
-                    });
+            //    this.$http.post('data.json', this.user)
+            //        .then(response => {
+            //            console.log(response);
+            //        }, error => {
+            //            console.log(error);
+            //        });
+                this.resource.save({}, this.user);
             },
             fetchData() {
-                this.$http.get('')
+                this.$http.get('data.json')
                     .then(response => {
                         return response.json();
                     })
@@ -57,6 +58,9 @@
                         this.users = resultArray;
                     });
             }
+        },
+        created() {
+            this.resource = this.$resource('data.json');
         }
     }
 </script>
