4b7a13698a014726edbfa3e62e509d3dd59580a2
dastangoo <hamed.dastangoo@gmail.com>
2020-05-02 14:57:27 +0430

    Module Project

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,11 +2,16 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded ID: {{ id }}</p>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
 <script>
     export default {
+        data() {
+            return {
+                id: this.$rout.params.id
+            }
         methods: {
             navigateToHome() {
                 <!--this.$router.push({path: 'path: '/'/'}); -->
