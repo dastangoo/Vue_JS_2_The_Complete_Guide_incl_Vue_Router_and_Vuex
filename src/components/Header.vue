88f2914aacc8fa6f0a5a863aff8962094a649072
dastangoo <hamed.dastangoo@gmail.com>
2020-05-02 09:32:14 +0430

    Module Project

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+<ul class="nav nav-pills">
+  <li class="nav-item" to="/"><router-link>Home</router-link></li>
+  <li class="nav-item" to="/user"><router-link>User</router-link></li>
+</ul>
+</template>
