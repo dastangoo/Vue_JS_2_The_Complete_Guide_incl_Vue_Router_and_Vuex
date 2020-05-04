b336ae03dcb64a9da057840002c234f6582ac0b5
dastangoo <hamed.dastangoo@gmail.com>
2020-05-04 15:39:56 +0430

    Module Project

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,8 +4,26 @@
         <p>Locale: {{ $route.query.locale }}</p>
         <p>Analytics: {{ $route.query.q }}</p>
         <hr>
-        <button class="btn btn-primary">Confirm</button>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
         <div style="height: 700px"></div>
         <p id="Data">Some extra Data</p>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                confirmed: false
+            }
+        },
+        beforeRouteLeave(to, from, next) {
+            next();
+            if(this.confirmed) {
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
+</script>
