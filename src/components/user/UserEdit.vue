829c362db35d97eb2badff7dd801445646d36859
dastangoo <hamed.dastangoo@gmail.com>
2020-05-04 12:59:35 +0430

    Module Project

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,8 +1,10 @@
 <template>
-    <h3>Edit the User</h3>
-    <p>User loaded has ID: {{ $route.params.id }}</p>
-    <router-link 
+    <div>
+        <h3>Edit the User</h3>
+        <p>User loaded has ID: {{ $route.params.id }}</p>
+        <router-link 
         tag="button" 
-        :to="'/user' + $route.params.id + '/edit'"
+        to="'/user' + $route.params.id + '/edit'"
         class="btn btn-primary">Edit User</router-link>
+    </div>
 </template>
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import UserStart from './components/user/UserStart.vue';
 import UserDetail from './components/user/UserDetail.vue';
 import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
-import Home from './components/Header.vue';
+import Header from './components/Header.vue';
 
 
 export const routes = [
@@ -19,4 +19,6 @@ export const routes = [
         { path: ':id', component: UserDetail },
         { path: ':id/edit', component: UserEdit, name: 'userEdit' }
     ]},
+    //{ path: '/redirectme', redirect: '/user' }
+    { path: '/redirectme', redirect: { name: 'home' }}
 ];
