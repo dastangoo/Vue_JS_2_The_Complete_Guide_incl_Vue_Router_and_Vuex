aa50e2392fb8f2d90a9a9eaca39661dfcb407a4f
dastangoo <hamed.dastangoo@gmail.com>
2020-05-08 19:08:44 +0430

    Module Project

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/Home.vue';
+import Porfolio from './components/portfolio/Portfolio.vue';
+import Stocks from './components/stocks/Stock.vue';
+
+export const routes = [
+    { path: '/', component: Home },
+    { path: '/portfolio', component: Proftolio },
+    { path: '/stocks', component: Stocks }
+];
