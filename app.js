3df8002dd02181733dbba39655d89616395b443c
dastangoo <hamed.dastangoo@gmail.com>
2020-06-02 17:44:52 +0430

    Time to Practice (3) - Reactive Properties (Solutoin)

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed: {
+            result: function() {
+                return this.value == 37 ? 'done' : 'not there yet;'
+            }
+        },
+        watch: {
+            result: function() {
+                var vm = this;
+                setTimeout(function() {
+                    vm.value = 0;
+                }, 5000);
+            }
         }
     });
