f6fb2534f36dc9bff205938876e256c4fa802289
dastangoo <hamed.dastangoo@gmail.com>
2020-06-03 13:06:44 +0430

    Adding User Signin (Login)

--- src/components/auth/signin.vue
+++ src/components/auth/signin.vue
@@ -1,102 +1,110 @@
 <template>
-  <div id="signin">
-    <div class="signin-form">
-      <form @submit.prevent="onSubmit">
-        <div class="input">
-          <label for="email">Mail</label>
-          <input
-                  type="email"
-                  id="email"
-                  v-model="email">
+    <div id="signin">
+        <div class="signin-form">
+            <form @submit.prevent="onSubmit">
+                <div class="input">
+                    <label for="email">Mail</label>
+                    <input
+                        type="email"
+                        id="email"
+                        v-model="email">
+                </div>
+                <div class="input">
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        v-model="password">
+                </div>
+                <div class="submit">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         </div>
-        <div class="input">
-          <label for="password">Password</label>
-          <input
-                  type="password"
-                  id="password"
-                  v-model="password">
-        </div>
-        <div class="submit">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
+import axios from '../../axios-auth';
+export default {
     data () {
-      return {
-        email: '',
-        password: ''
-      }
+        return {
+            email: '',
+            password: ''
+        }
     },
     methods: {
-      onSubmit () {
-        const formData = {
-          email: this.email,
-          password: this.password,
+        onSubmit () {
+            const formData = {
+                email: this.email,
+                password: this.password,
+            }
+            console.log(formData)
+            axios.post('/accounts:signInWithPassword?key=AIzaSyAZGzlY3Pn7T6Yo7y7SMQn4JQY1d66Hl7Q', {
+                email: formData.email,
+                password: formData.password,
+                returnSecureToken: true
+            })
+            .then(res => console.log(res))
+            .catch(error => console.log(error))
         }
-        console.log(formData)
-      }
     }
-  }
+}
 </script>
 
 <style scoped>
-  .signin-form {
+.signin-form {
     width: 400px;
     margin: 30px auto;
     border: 1px solid #eee;
     padding: 20px;
     box-shadow: 0 2px 3px #ccc;
-  }
+}
 
-  .input {
+.input {
     margin: 10px auto;
-  }
+}
 
-  .input label {
+.input label {
     display: block;
     color: #4e4e4e;
     margin-bottom: 6px;
-  }
+}
 
-  .input input {
+.input input {
     font: inherit;
     width: 100%;
     padding: 6px 12px;
     box-sizing: border-box;
     border: 1px solid #ccc;
-  }
+}
 
-  .input input:focus {
+.input input:focus {
     outline: none;
     border: 1px solid #521751;
     background-color: #eee;
-  }
+}
 
-  .submit button {
+.submit button {
     border: 1px solid #521751;
     color: #521751;
     padding: 10px 20px;
     font: inherit;
     cursor: pointer;
-  }
+}
 
-  .submit button:hover,
-  .submit button:active {
+.submit button:hover,
+.submit button:active {
     background-color: #521751;
     color: white;
-  }
+}
 
-  .submit button[disabled],
-  .submit button[disabled]:hover,
-  .submit button[disabled]:active {
+.submit button[disabled],
+.submit button[disabled]:hover,
+.submit button[disabled]:active {
     border: 1px solid #ccc;
     background-color: transparent;
     color: #ccc;
     cursor: not-allowed;
-  }
+}
 </style>
