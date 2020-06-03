f9f29dea62307c23f07e842730893197756d7ff0
dastangoo <hamed.dastangoo@gmail.com>
2020-06-03 18:27:48 +0430

    Accessing other Resources from Vuex

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -1,45 +1,32 @@
 <template>
-  <div id="dashboard">
-    <h1>That's the dashboard!</h1>
-    <p>You should only get here if you're authenticated!</p>
-    <p>Your email address: {{ email }}</p>
-  </div>
+    <div id="dashboard">
+        <h1>That's the dashboard!</h1>
+        <p>You should only get here if you're authenticated!</p>
+        <p>Your email address: {{ email }}</p>
+    </div>
 </template>
 
 <script>
-  import axios from 'axios';
+import axios from 'axios';
 
-  export default {
-    data () {
-      return {
-        email: ''
-      }
+export default {
+    computer: {
+        email() {
+            return this.$store.getters.user.email;
+        }
     },
     created () {
-      axios.get('/users.json')
-        .then(res => {
-          console.log(res)
-          const data = res.data
-          const users = []
-          for (let key in data) {
-            const user = data[key]
-            user.id = key
-            users.push(user)
-          }
-          console.log(users)
-          this.email = users[0].email
-        })
-        .catch(error => console.log(error))
+        this.$store.dispatch('fetchUser');
     }
-  }
+}
 </script>
 
 <style scoped>
-  h1, p {
+h1, p {
     text-align: center;
-  }
+}
 
-  p {
+p {
     color: red;
-  }
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -2,22 +2,28 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import axios from './axios-auth';
+import globalAxios from 'axios';
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
         idToken: null,
-        userId: null
+        userId: null,
+        user: null
     },
     mutations: {
         authUser(state, userData) {
             state.idToken = userData.token;
             state.userId = userData.userId;
+        },
+        storeUser(state, user) {
+            state.user = user;
         }
     },
     actions: {
-        signup({commit}, authData) {
+        signup({commit, dispatch}, authData) {
             axios.post('/accounts:signUp?key=AIzaSyAZGzlY3Pn7T6Yo7y7SMQn4JQY1d66Hl7Q', {
                 email: authData.email,
                 password: authData.password,
@@ -29,6 +35,7 @@ export default new Vuex.Store({
                     token: res.data.idToken,
                     userId: res.data.localId
                 });
+                dispatch('storeUser', authData);
             })
             .catch(error => console.log(error))
         },
@@ -46,9 +53,32 @@ export default new Vuex.Store({
                 });
             })
             .catch(error => console.log(error))
+        },
+        storeUser({commit}, userData) {
+            globalAxios.post('/users.json', userData)
+            .then(rest => console.log(res))
+            .catch(error => console.log(error));
+        },
+        fetchUser({commit}) {
+            globalAxios.get('/users.json')
+            .then(res => {
+                console.log(res)
+                const data = res.data
+                const users = []
+                for (let key in data) {
+                    const user = data[key]
+                    user.id = key
+                    users.push(user)
+                }
+                console.log(users)
+                commit('storeUser', users[0]);
+            })
+            .catch(error => console.log(error));
         }
     },
     getters: {
-
+        user(state) {
+            return state.user;
+        }
     }
 })
